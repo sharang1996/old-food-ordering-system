02433fc229c4ceb445f976dfedf243b66734b340
sharang <sharanggupta96@gmail.com>
2022-10-13 08:07:37 +0100

    Added input and output ports for the order application service

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/RestaurantRepository.java
@@ -0,0 +1,7 @@
+package com.food.ordering.system.order.service.domain.ports.output.repository;
+
+import com.food.ordering.system.order.service.domain.entity.Restaurant;
+
+public interface RestaurantRepository {
+    Restaurant findRestaurantInformation(Restaurant restaurant);
+}
